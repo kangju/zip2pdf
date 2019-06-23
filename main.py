a5a5daf68832f4248309d816e5251031e3eafba8
kangju <kangju0815@gmail.com>
2019-06-23 16:10:43 +0900

    get img list

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import Path from pathlib 
+from pathlib import Path
 
 def main():
     IMG_FOLDER = "/Users/kangju/Work/program/zip2pdf/sample"
 
-    print([s for s in ])
+    imgList = list([str(s) for s in Path(IMG_FOLDER).glob("*.jpg")])
+    imgList.sort()
+    print(imgList)
 
 if __name__ == "__main__":
     main()
