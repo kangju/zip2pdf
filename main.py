849f32582130d22f724385b024a1fa0ad4e46773
kangju <kangju0815@gmail.com>
2019-07-07 00:51:30 +0900

    add convert zip to pdf

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
 from pathlib import Path
+import shutil
+import os
+import zipfile
+import img2pdf
+import datetime
+import sys
+
+#memo
+#設定をJSON化
+
+
 
 def main():
-    IMG_FOLDER = "/Users/kangju/Work/program/zip2pdf/sample"
 
-    imgList = list([str(s) for s in Path(IMG_FOLDER).glob("*.jpg")])
+    argv = sys.argv
+    if len(argv) > 2:
+        print("ZIPファイルパスを指定してください")
+        return 
+
+    ZIP_PATH = argv[1]
+    ZIP_FILE_NAME = str(os.path.basename(ZIP_PATH)).replace(" ","")[:-4]
+    PDF_FILE_PATH = ZIP_FILE_NAME+".pdf"
+
+    print("unzip:" + ZIP_PATH)
+    imgfolder = unzip(ZIP_PATH)
+    print("unziped:" + ZIP_PATH)
+
+    print("Convert images to PDF")
+    createPdf(imgfolder,PDF_FILE_PATH)
+    print("Converted images to PDF:" + PDF_FILE_PATH)
+
+def unzip(zipPath):
+    UNZIP_FOLDER = "/Users/kangju/Work/program/zip2pdf/sample/temp"
+    unzipFolderNameCache = ""
+
+    with zipfile.ZipFile(zipPath) as extZip:
+        unzipFolderNameCache = extZip.namelist()[0]
+        extZip.extractall(UNZIP_FOLDER)
+
+    return os.path.join(UNZIP_FOLDER,unzipFolderNameCache)
+
+def createPdf(folderpath,createPdfPath):
+    imgList = list([str(s) for s in Path(folderpath).glob("*.jpg")])
     imgList.sort()
-    print(imgList)
+
+    with open(createPdfPath,"wb") as f:
+        f.write(img2pdf.convert(list(imgList)))
 
 if __name__ == "__main__":
     main()
